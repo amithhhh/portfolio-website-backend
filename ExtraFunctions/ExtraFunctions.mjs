5bd4f3182b29b29c1c47620fada8f9743dd79dbf
AMITH E K <amithajith7025@gmail.com>
2025-03-31 10:43:20 +0530

    updated tokens

--- ExtraFunctions/ExtraFunctions.mjs
+++ ExtraFunctions/ExtraFunctions.mjs
@@ -3,9 +3,6 @@ import axios from "axios";
 export const sendTelegramNotification = async (message) => {
     const botToken = process.env.TELEGRAM_BOT_TOKEN;
     const chatId = process.env.TELEGRAM_CHAT_ID;
-    console.log("Bot Token:", process.env.TELEGRAM_BOT_TOKEN);
-    console.log("Chat ID:", process.env.TELEGRAM_CHAT_ID);
-
     const text = `📩 *New Message Received:*\n\n👤 Name: ${message.name}\n📧 Email: ${message.email}\n📝 Subject: ${message.subject}\n💬 Message: ${message.message}`;
 
 
--- config/db.mjs
+++ config/db.mjs
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("Mongo Db connected");
+        //console.log("Mongo Db connected");
     } catch (error) {
         console.error(error);
         process.exit(1)
